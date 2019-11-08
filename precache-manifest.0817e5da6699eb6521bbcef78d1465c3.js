31dedf6e6dc82c0926863722257a0f497ddaa579
Amareis <terma95@gmail.com>
2019-11-08 10:15:16 +0000

    Deploying to gh-pages from master adcd072abeeb8fa8fd63bcb3526ae07c6e41454c

---
+++ precache-manifest.0817e5da6699eb6521bbcef78d1465c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5920b8b48273caeff11d31ca78a877b5",
+    "url": "/vk-want-to-touch/index.html"
+  },
+  {
+    "revision": "cd7e89acd416d30ef6ab",
+    "url": "/vk-want-to-touch/static/css/2.27cde75d.chunk.css"
+  },
+  {
+    "revision": "cd7e89acd416d30ef6ab",
+    "url": "/vk-want-to-touch/static/js/2.38d57401.chunk.js"
+  },
+  {
+    "revision": "e1f237e42327a9aaad6d",
+    "url": "/vk-want-to-touch/static/js/main.9631759c.chunk.js"
+  },
+  {
+    "revision": "82a2c462ee522dba389b",
+    "url": "/vk-want-to-touch/static/js/runtime-main.6b0f0492.js"
+  }
+]);
